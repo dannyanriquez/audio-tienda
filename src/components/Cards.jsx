96b841c11e099b063467ca240e2459d4a886c358
Danny Anriquez <anriquezda@gmail.com>
2021-10-07 09:48:01 -0300

    add title

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,9 +6,23 @@ import {Card, Button} from 'react-bootstrap'
 export const Cards = () => {
     return (
         <section>
+            <div className="container text-center my-5">
+                <div className="row">
+                <h1 className="col-md-12">
+                    SOMOS UNA TIENDA 100% ONLINE. 
+                    TE OFRECEMOS VARIEDAD Y CALIDAD ACORDE PARA LOS 
+                    USUARIOS MÁS EXIGENTES
+                </h1>
+                <h6 className="col-md-12 my-5">
+                    Representantes en Argentina de productos de Harman, Bose & Peavey, 
+                    Contamos con un stock constante, el beneficio de 
+                    comprar en cuotas y recibirlo GRATIS en tu casa, hacemos envíos a todo el país.
+                </h6>
+                </div>
+            </div>
             <div className="container my-5">
                 <div className="row">
-                    <div className="col-md-3">
+                    <div className="col-md-4">
                         <Card style={{ width: '18rem' }}>
                             <Card.Img variant="top" src="./images/EonOneCompact/JBL_EonOneCompact_withTablet_original.png"/>
                             <Card.Body>
@@ -17,11 +31,11 @@ export const Cards = () => {
                                 Some quick example text to build on the card title and make up the bulk of
                                 the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Comprar</Button>
+                                <Button variant="dark">Comprar</Button>
                             </Card.Body>
                         </Card>
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-4">
                         <Card style={{ width: '18rem' }}>
                             <Card.Img variant="top" src="./images/LSR305/mkii305-angle_z_original.jpeg"/>
                             <Card.Body>
@@ -30,11 +44,11 @@ export const Cards = () => {
                                 Some quick example text to build on the card title and make up the bulk of
                                 the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Comprar</Button>
+                                <Button variant="dark">Comprar</Button>
                             </Card.Body>
                         </Card>
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-md-4">
                         <Card style={{ width: '18rem' }}>
                             <Card.Img variant="top" src="./images/PRX818XLFW/prx818xlfw_front_grille_z_original.png"/>
                             <Card.Body>
@@ -43,20 +57,7 @@ export const Cards = () => {
                                 Some quick example text to build on the card title and make up the bulk of
                                 the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Comprar</Button>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div className="col-md-3">
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="./images/Eon208P/eon208p-open-angle_z_original.png"/>
-                            <Card.Body>
-                                <Card.Title>EON208P portable</Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the bulk of
-                                the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Comprar</Button>
+                                <Button variant="dark">Comprar</Button>
                             </Card.Body>
                         </Card>
                     </div>
